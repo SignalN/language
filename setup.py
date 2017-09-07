2f0012fbd66d44e0381d28112e9a4c9d652ed936
bittlingmayer <adam+code@signaln.com>
2017-09-07 20:55:45 +0400

    Add submodules, licence and classifiers to setup

--- setup.py
+++ setup.py
@@ -2,13 +2,21 @@ from setuptools import setup
 
 setup(
   name = 'language',
-  packages = ['language'],
-  version = '0.2',
+  packages = ['language.chars', 'language.tokens', 'language.ngrams'],
+  version = '0.5',
   description = 'Basic tools for working with natural language text data',
   author = 'Signal N',
   author_email = 'language@signaln.com',
   url = 'https://github.com/signaln/language',
-  download_url = 'https://github.com/signaln/language/archive/0.2.tar.gz',
+  download_url = 'https://github.com/signaln/language/archive/0.5.tar.gz',
   keywords = ['natural language processing', 'text', 'language', 'ngrams'],
-  classifiers = [],
+  license='MIT',
+  classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+    ],
 )
