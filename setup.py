5c50c78f65bcc2c999b44d530e7412185248352d
bittlingmayer <adam+code@signaln.com>
2017-09-08 11:32:04 +0400

    Fix version mismatch

--- setup.py
+++ setup.py
@@ -3,12 +3,12 @@ from setuptools import setup
 setup(
   name = 'language',
   packages = ['language'],
-  version = '0.5',
+  version = '0.6',
   description = 'Basic tools for working with natural language text data',
   author = 'Signal N',
   author_email = 'language@signaln.com',
   url = 'https://github.com/signaln/language',
-  download_url = 'https://github.com/signaln/language/archive/0.5.tar.gz',
+  download_url = 'https://github.com/signaln/language/archive/0.6.tar.gz',
   keywords = ['natural language processing', 'text', 'language', 'ngrams'],
   license='MIT',
   classifiers=[
